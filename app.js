7f407596c497ea5518b05de775db2c232be2b161
mohmedeprahem <m.eprahem.168@gmail.com>
2023-05-03 21:24:00 +0200

    Create structure of app using MVC model and connect to database

---
+++ app.js
@@ -0,0 +1,16 @@
+// package requirement
+const express = require('express');
+const mongoose = require('mongoose')
+
+// inti express
+const app = express();
+
+// Connect to DataBase
+mongoose.connect(`mongodb+srv://mohmed123:mohmed123@board-game-shop.vsepsuu.mongodb.net/e_commerce`)
+.then(() => console.log(`Connect to database....`))
+
+// Connect to server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`connect to server at port: ${port}...`)
+})
