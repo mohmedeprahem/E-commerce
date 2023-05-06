e4cc2d5eecca8c93329b5e0a4dd56703758065c9
mohmedeprahem <m.eprahem.168@gmail.com>
2023-05-06 16:01:07 +0200

    Create product schema in models/product.js file

---
+++ models/product.js
@@ -0,0 +1,50 @@
+// Package requirement
+const mongoose = require('mongoose')
+
+// build product schema
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 50,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 100000,
+  },
+  imgs: {
+    type: [String],
+    required: true
+  },
+  colors: {
+    type: [String],
+    required: true
+  },
+  sizes: {
+    type: [String],
+    required: true
+  },
+  productInfo: {
+    type: [String],
+    required: true
+  },
+  available: {
+    type: Boolean,
+    default: true
+  },
+  rateCounter: {
+    type: Number,
+    default: 0
+  },
+  starRating: {
+    type: Number,
+    default: 0
+  }
+}, { timestamps: true })
+
+const product = mongoose.model('Porduct', productSchema);
+
+module.exports = product
