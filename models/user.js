df1f5d1103c0839a895e5fcf3fc3220685851a1c
mohmedeprahem <m.eprahem.168@gmail.com>
2023-05-06 15:34:54 +0200

    Edit user schema to add inside it address of user

--- models/user.js
+++ models/user.js
@@ -1,5 +1,54 @@
 // Package requirement
 const mongoose = require('mongoose');
+
+// Define the sub-document in user schema
+const addressSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+    maxLength: 50
+  },
+  physicalAddress: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 40
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 40
+  },
+  apartmentNumber: Number,
+  city: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 30
+  },
+  governorate:{
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 30
+  },
+  postalCode: {
+    type: Number,
+    required: ture
+  },
+  phoneNumber: {
+    type: Number,
+    required: ture
+  }
+}, { timestamps: true });
+
+// Define the user collections Schema
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -25,8 +74,11 @@ const userSchema = new mongoose.Schema({
     expirationYear: Number,
     CardNames: String,
   },
-  timeStamps: true
-});
+  addresses: {
+    type: [addressSchema],
+    required: false
+  }
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
